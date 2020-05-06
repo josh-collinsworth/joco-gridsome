88138131f9c9c97ad0a56342d8666a170eceab21
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2020-05-05 22:37:10 -0500

    Add form submission success page

---
+++ src/pages/Success.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Message received</h1>
+    <p class="fancy">Thanks for reaching out!</p>
+    <p>I'll do my best to get back to you ASAP.</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
