f133dd4f243855d5e1941d05581c984e6247c3cf
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2020-05-05 22:32:38 -0500

    Experimenting with Netlify forms/Vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,15 +1,67 @@
 <template>
-	<div>
-		<h1>Get in touch</h1>
-		<p class="fancy">Drop a line if you'd like.</p>
-		<form name="contact" method="POST" data-netlify="true">
-			<label>Your Name: <input type="text" name="name" /></label>
-			<label>Your Email: <input type="email" name="email" /></label>
-			<label>Message: <textarea name="message"></textarea></label>
-			<button type="submit">Send</button>
-		</form>
-	</div>
+	<form
+		name="contact"
+		method="post"
+		v-on:submit.prevent="handleSubmit"
+		action="/success/"
+		data-netlify="true"
+		data-netlify-honeypot="bot-field"
+	>
+		<input type="hidden" name="form-name" value="contact" />
+		<p hidden>
+			<label>
+				Don’t fill this out: <input name="bot-field" />
+			</label>
+		</p>
+		<div class="sender-info">
+			<div>
+				<label for="name" class="label" >Your name</label>
+				<input type="text" name="name" v-model="formData.name" />
+			</div>
+			<div>
+				<label for="email">Your email</label>
+				<input type="email" name="email" v-model="formData.email" />
+			</div>
+		</div>
+
+		<div class="message-wrapper">
+			<label for="message">Message</label>
+			<textarea name="message" v-model="formData.message"></textarea>
+		</div>
+
+		<button type="submit">Submit form</button>
+	</form>
 </template>
+
+<script>
+export default {
+
+	data() {
+		return {
+			formData: {},
+  	}
+	},
+	methods: {
+		encode(data) {
+			return Object.keys(data)
+				.map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+				.join('&')
+		},
+		handleSubmit(e) {
+			fetch('/', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				body: this.encode({
+					'form-name': e.target.getAttribute('name'),
+					...this.formData,
+				}),
+			})
+			.then(() => this.$router.push('/success'))
+			.catch(error => alert(error))
+		}
+	}
+}
+</script>
 
 <style lang="scss">
 	form {
