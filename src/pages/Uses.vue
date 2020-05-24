9024982f3ecc3aa62f538c022deb2b324e08f8f9
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2020-05-24 16:27:40 -0500

    Uses page in progress; pre-XA replacement

--- src/pages/Uses.vue
+++ src/pages/Uses.vue
@@ -1,23 +1,185 @@
 <template>
   <Layout>
-    <h1>{{ $page.wordPressPage.title }}</h1>
 
-    <div v-html="$page.wordPressPage.content">
-    </div>
+
+
+
+			<div class="heading-wrapper"><h1>Uses</h1></div>
+
+
+<p>Things that I use and like, professionally or otherwise.</p>
+
+
+
+<div class="heading-wrapper"><h2>Development</h2></div>
+
+
+
+<ul>
+  <li>
+    <strong><ExternalLink to="https://code.visualstudio.com/">Visual Studio Code</ExternalLink></strong>:
+    my favorite editor ever.
+    I was a reluctant Sublime convert but the switch was more than worth it.
+    <ul>
+      <li>
+        <strong>Theme: </strong>
+        I use the built-in <strong><ExternalLink to="https://marketplace.visualstudio.com/items?itemName=monokai.theme-monokai-pro-vscode">Monokai</ExternalLink></strong> theme.
+      </li>
+      <li>
+        <strong>Font: <ExternalLink to="https://www.monolisa.dev/">MonoLisa</ExternalLink></strong>.
+        I’m a code nerd and a font nerd. This is my favorite dev font. (If you’re not willing to spend the money though—and I definitely understand if that’s the case—
+        <strong><ExternalLink to="https://github.com/tonsky/FiraCode">Fira Code</ExternalLink></strong> is my runner-up.)
+        </li>
+      <li>
+        <strong>Extensions</strong>:
+        I don’t have too many global recommendations here (it depends a lot on what you’re working with), but I’ll mention:<ul><li>
+        <strong><ExternalLink to="https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer">Bracket Pair Colorizer</ExternalLink></strong>:
+        makes it easier to distinguish where a code block ends by assigning the same color to the opening and closing brackets.
+      </li>
+      <li>
+        <strong><ExternalLink to="https://marketplace.visualstudio.com/items?itemName=pnp.polacode">Polacode</ExternalLink></strong>:
+        a nifty, pretty way to take code screenshots
+      </li>
+      <li>
+        <strong><ExternalLink to="https://marketplace.visualstudio.com/items?itemName=MS-vsliveshare.vsliveshare">Live Share</ExternalLink></strong>:
+        the best way to collaborate on code remotely that I’ve ever used.
+      </li>
+      <li>
+        <strong><ExternalLink to="https://marketplace.visualstudio.com/items?itemName=Tobermory.es6-string-html">es6-string-html</ExternalLink></strong>:
+        treats HTML inside template literal strings like actual HTML code, for the purposes of highlighting, code completion, Emmett, etc.
+      </li>
+      <li>
+        <strong><ExternalLink to="https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens">GitLens</ExternalLink></strong>:
+        less indispensable now that VS Code has the <ExternalLink to="https://scotch.io/bar-talk/vs-code-v144-adds-a-git-timeline-view">Timeline view</ExternalLink>, but still amazingly handy for viewing git status and history in projects.
+      </li>
+      <li>
+        <strong><ExternalLink to="https://marketplace.visualstudio.com/items?itemName=Compulim.compulim-vscode-closetag">Close HTML/XML Tag</ExternalLink></strong>:
+        the only keyboard shortcut I miss from Sublime Text is easily replicated with this extension. Gives you a key command to auto-close whatever HTML tag your cursor is inside of.
+      </li>
+    </ul>
+    </li>
+</ul>
+</li>
+  <li>
+    <strong><ExternalLink to="https://localwp.com/">Local</ExternalLink></strong>:
+    hands-down the best way to develop WordPress sites. Fast and easy (especially if you host with Flywheel or WP Engine; you can push and pull sites directly).
+    </li>
+  <li>
+    <strong><ExternalLink to="https://codekitapp.com/">CodeKit</ExternalLink></strong>:
+    there’s nothing CodeKit does that I couldn’t accomplish by fiddling with NPM scripts, but why bother? It’s smooth and easy to use, and as a bonus, it works great with Local.
+    </li>
+  <li>
+    <strong><ExternalLink to="https://www.iterm2.com/">iTerm2</ExternalLink></strong>:
+    I’ve tried other terminals, but none seem to run as well, and with the recent update, arguably, none look better, either.
+    </li>
+  <li>
+    <strong><ExternalLink to="https://www.netlify.com/">Netlify</ExternalLink></strong>:
+    I’m also a fan of
+  <strong><ExternalLink to="https://vercel.com/home">Now</ExternalLink></strong>, but Netlify has taken over my JAMstack hosting #1 spot. It’s free to start and unbelievably easy. Netlify makes the painfully difficult effortlessly simple.
+  </li>
+</ul>
+
+
+
+<h3>Languages and frameworks</h3>
+
+<ul>
+  <li>JavaScript is my main language these days, and
+    <strong><ExternalLink to="https://vuejs.org/">Vue</ExternalLink> </strong>is my main go-to for anything I’m building (I’m unsurprisingly, therefore, a huge fan of
+  <strong><ExternalLink to="https://nuxtjs.org">Nuxt</ExternalLink></strong> and
+  <strong><ExternalLink to="https://gridsome.org/">Gridsome</ExternalLink></strong> as well).
+  </li>
+  <li>I’m also a big fan of
+    <strong><ExternalLink to="https://svelte.dev/">Svelte</ExternalLink></strong>, and I know
+  <strong><ExternalLink to="https://reactjs.org/">React</ExternalLink></strong> as well, which is also cool but it’s not my favorite.
+  </li>
+  <li>I love
+    <strong>CSS</strong> passionately, but I don’t use any libraries or frameworks; all my CSS is from-scratch and custom (usually written in
+  <strong><a href="https://sass-lang.com/">SCSS</ExternalLink></strong>).
+  </li>
+  <li>I’m a <ExternalLink to="https://wordpress.org/">
+  <strong>WordPress</strong></ExternalLink> expert and generally prefer to build themes from scratch (as with this one).
+  </li>
+  <li>Professionally, I work in Ruby on Rails with SCSS and HAML templates using Vue components.
+
+  </li>
+</ul>
+
+
+
+<div class="heading-wrapper"><h2>General</h2></div>
+
+
+
+<ul><li>
+  <strong><a href="https://www.mozilla.org/en-US/firefox/new/" rel="noreferrer noopener">Firefox</ExternalLink></strong>:
+  currently in the middle of trying to convert again after failing last time. Liking what I see so far. (<a href="https://www.google.com/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=1&amp;cad=rja&amp;uact=8&amp;ved=2ahUKEwji7e3c_PzoAhXPZc0KHazlCD4QFjAAegQIERAC&amp;url=https%3A%2F%2Fbrave.com%2F&amp;usg=AOvVaw2CfcgN6wLi3270uJRtAJ62" rel="noreferrer noopener">
+<strong>Brave</strong></ExternalLink> is my fallback choice.)
+</li>
+<li>
+  <strong><a href="https://www.getcloudapp.com/" rel="noreferrer noopener">CloudApp</ExternalLink></strong>:
+  taking screenshots and videos is a many-times-daily activity for me, and CloudApp makes it as seamless as possible.
+  </li>
+<li>
+  <strong><a href="https://rightfontapp.com/" rel="noreferrer noopener">RightFont</ExternalLink></strong>:
+  a simple, minimalist app for managing your computer’s fonts that looks and works better than Mac’s built-in FontBook.
+  </li>
+<li>
+  <strong><ExternalLink to="https://www.anker.com/products/variant/ExternalLinknker-24g-wireless-vertical-ergonomic-optical-mouse/ExternalLink7852011">Anker 2.4G Wireless Vertical Ergonomic Optical Mouse</ExternalLink></strong>:
+  I could feel my wrist straining using both a magic mouse and a touchpad. I switched to this mouse years ago. It took some adjustment, but I don’t ever want to use another mouse again, and I don’t even use the extra buttons.
+  </li>
+</ul>
+
+
+
+<div class="heading-wrapper"><h2>Productivity &amp; Utility</h2></div>
+
+
+
+<ul><li><ExternalLink to="http://ExternalLinklfredapp.com">
+<strong>Alfred</strong></ExternalLink>: the first thing I install on every new Mac. (I use the <ExternalLink to="https://www.alfredapp.com/powerpack/">powerpack</ExternalLink> for added features, which is worth every penny.) The main features I love are clipboard history, snippet expansion, global search (never touch Finder again!), and workflows to automate repetitive or tedious tasks. Wouldn’t ever want to work without it.
+</li>
+<li>
+  <strong><ExternalLink to="https://rectangleapp.com/">Rectangle</ExternalLink></strong>:
+  never drag or resize another window again. Keyboard shortcuts to put everything right where you need it.
+  </li>
+<li>
+  <strong><ExternalLink to="https://www.macbartender.com/">Bartender</ExternalLink></strong>:
+  helps wrangle all those app icons in your taskbar if you’re a minimalist like me.
+  </li>
+<li>
+  <strong><ExternalLink to="https://matthewpalmer.net/rocket/">Rocket</ExternalLink></strong>:
+  lends you Slack-like ability to use emoji anywhere. Way better than the OSX default emoji picker.
+  </li>
+<li>
+  <strong><ExternalLink to="https://www.wacom.com/en-us/products/pen-tablets/intuos-pro-small">Notion</ExternalLink></strong>:
+  my current favorite notes app. Can grow and get as complex as you need it to be, but for simple notes, the markdown and code highlight support is fantastic.
+  </li>
+<li>
+  <strong><ExternalLink to="https://1password.com/">1Password</ExternalLink></strong>:
+  I’ve tried and liked other password software, but 1Password is just the most reliable cross-platform and -device in my experience.
+  </li>
+</ul>
+
+
+
+<div class="heading-wrapper"><h2>Design and Illustration</h2></div>
+
+
+
+<ul><li>
+  <strong><ExternalLink to="https://ExternalLinkffinity.serif.com">Affinity Photo, Designer and Publisher</ExternalLink></strong>:
+  you can have software 90% as good as Photoshop, Illustrator and InDesign <em>forever</em> for the cost of three months of Creative Cloud.
+  </li>
+<li>
+  <strong><a href="https://www.wacom.com/en-us/products/pen-tablets/intuos-pro-small" rel="noreferrer noopener">Wacom Intuos Pro PTH-451</ExternalLink></strong> (small): I don’t often illustrate these days, but when I do, I’m always glad I have this fantastic pad. Wireless is a big plus.
+  </li>
+</ul>
+
+
 
   </Layout>
 </template>
-
-
-<page-query>
-query page {
-	wordPressPage(id:4412) {
-    title
-    content
-  }
-}
-</page-query>
-
 
 <script>
 export default {
@@ -31,9 +193,7 @@ export default {
     title: "Code"
   },
 }
-</script>
-
-<style>
+</script><style>
   .loadfade {
       transition: all .3s ease;
   }
