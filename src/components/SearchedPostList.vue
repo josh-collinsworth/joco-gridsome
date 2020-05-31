ee615dd2203944e70364af1b39228b0d344ec700
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2020-05-31 12:12:03 -0500

    Blog pagination fixed; footer links added

---
+++ src/components/SearchedPostList.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<article v-for="post in posts" :key="post.node.id">
+			<g-link :to="post.node.slug">
+				<img :src="post.node.featuredMedia.sourceUrl" alt="">
+				<span class="sr">{{ post.node.title }}</span>
+			</g-link>
+			<h2>
+				<g-link :to="post.node.slug">
+					<span  v-html="post.node.title"></span>
+				</g-link>
+			</h2>
+			<div v-html="post.node.excerpt"></div>
+		</article>
+	</div>
+</template>
+
+
+<script>
+export default {
+	props: {
+		posts: {
+			type: Array
+		}
+	}
+}
+</script>
+
+
+<style lang="scss" scoped>
+article img {
+  margin-top: 6rem;
+}
+</style>
