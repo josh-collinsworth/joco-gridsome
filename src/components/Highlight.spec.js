c5cc7511c1c538ec01341d0dabbfec1197ab1b93
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2021-01-30 21:44:00 -0600

    Updating some file formatting nits

--- src/components/Highlight.spec.js
+++ src/components/Highlight.spec.js
@@ -6,13 +6,13 @@ describe('<Highlight>', () => {
 
   it('renders properly', () => {
     const text = '<span>Hello!</span>'
-    
+
     const component = mount(Highlight, {
       slots: {
         default: text
       }
     })
-    
+
     expect(component.element.classList.contains('highlight')).to.be.true
     expect(component.text()).to.equal('Hello!')
   })
